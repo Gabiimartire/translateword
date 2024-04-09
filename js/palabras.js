e385c59e20f6f3a6d31656afb5d6f5b5b2cd6de2
Gabiimartire <gabbiimartire3@gmail.com>
2024-04-09 03:54:12 -0300

    avance

---
+++ js/palabras.js
@@ -0,0 +1,30 @@
+var palabras = [
+    {
+        "engTerm": "Arrest",
+        "spaEquiv": "Detencion",
+        "definition": "To deprive(a person) of liberty by taking them into custody, under lawful authory.",
+        "sampleSentenceEng": "After your son's arrest, he will be taken to the county jail and will be held there until he appears before the judge",
+        "sampleSentenceSpa": "Después del arresto de su hijo, él será llevado a la cárcel del condado y permanecerá ahí hasta que se presente ante un juez"
+    },
+    {
+        "engTerm": "Assault",
+        "spaEquiv": "Agresion",
+        "definition": "An intentional or recless act that causes another person to expect to be subjected to immediate and unlawful violence.",
+        "sampleSentenceEng": "I need to know when the assault took place and if you recognize the assailant.",
+        "sampleSentenceSpa": "Necesito saber cuándo ocurrió la agresión física y si usted reconoce al agresor"
+    },
+    {
+        "engTerm": "Bail",
+        "spaEquiv": "Fianza",
+        "definition": "Security, usually a sum of money exchanged for the release of an arrested person as a guarantee of that person's appearance for trial",
+        "sampleSentenceEng": "Your bail has been set at $25.000. If you don't have the funds, you can see a ball bondsman.",
+        "sampleSentenceSpa": "Su fianza está fijada a $25.000. Si no tiene los fondos, puede ver un fiador judicial."
+    },
+    {
+        "engTerm": "Blood alcohol level",
+        "spaEquiv": "Nivel de alcoholemia",
+        "definition": "Refers to the concentration of alcohol in one's bloodstream, expressed as a percentage. This percentage isused to determine whether a person is legally intoxicated, especially for DUI law",
+        "sampleSentenceEng": "Your blood alcohol level exceeds 0.08%.",
+        "sampleSentenceSpa": "Su nivel de alcoholemia excede 0.08%"
+    }
+]
